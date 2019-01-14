2a921f97590932ffe0788c8c8d5fb0a16901cb7e
AExiaoliou <aexiaoliou@qq.com>
2019-01-14 14:23:22 +0800

    some annotations

--- Sunday/Sunday.py
+++ Sunday/Sunday.py
@@ -9,18 +9,19 @@ class Sunday:
     def __init__(self, pattern):
         self.__pattern = pattern
         self.__length = len(pattern)
-        for i in range(self.__length):
+        for i in range(self.__length): # get shift table
             self.__shift[pattern[i]] = self.__length - i
     
     def search(self, mainStr):
         s = mainStr
+        # __ is shit
         length = self.__length
         pattern = self.__pattern
         shift = self.__shift
         i = j = 0
-        while j < len(s): 
+        while j < len(s):
             if s[j] != pattern[i]:
-                j = j - i; i = 0
+                j = j - i; i = 0 # let sentinel revent to the head of str and pattern
                 if s[j + length] in shift:
                     j += shift[s[j + length]]
                 else:
@@ -29,7 +30,11 @@ class Sunday:
                 i += 1
                 j += 1
                 if i == length:
-                    return j - i
+                    return j - i 
+                '''
+                if you need a multiply match
+                just change return to an array
+                '''
         return None
 
 searcher = Sunday('abc')
