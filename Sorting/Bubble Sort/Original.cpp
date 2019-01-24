c622c63cc635a312aa7d9df9293830a434490867
AExiaoliou <aexiaoliou@qq.com>
2019-01-25 00:58:05 +0800

    Update Original.cpp

--- Sorting/Bubble Sort/Original.cpp
+++ Sorting/Bubble Sort/Original.cpp
@@ -1,1 +1,6 @@
-
+void Swaper(int& a, int& b) {}//suppose we have a swaper
+void BubbleSort(int a[], int len) {
+	for (int i = 0; i < len - 1; i++) 
+		for (int j = i + 1; j < len; j++) 
+			if (a[i] > a[j]) Swaper(a[i], a[j]);
+}
