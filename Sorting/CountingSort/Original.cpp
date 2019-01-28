5b46dec1fbde13f9328ee29b4318a0313c4dd2cf
AExiaoliou <aexiaoliou@qq.com>
2019-01-29 00:36:09 +0800

    Update Original.cpp

--- Sorting/CountingSort/Original.cpp
+++ Sorting/CountingSort/Original.cpp
@@ -1,1 +1,16 @@
-
+int* countingSort(int[] a, int len) {
+    int counter[10000];
+    memset(counter, 0, sizeof(counter));
+    for (int i = 0; i < len; i++) {
+        counter[a[i]]++;
+    }
+    int* res = new int [n];
+    for (int i = 0, j = 0; i < 10000 && j < len; ){
+        if (counter[i] > 0) {
+            arr[j] = i;
+            counter[i]--;
+            j++;
+        }else i++;
+    }
+    return res;
+}
