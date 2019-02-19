be78ce522edd4ded12c414b2d2237fd38823d3df
AExiaoliou <aexiaoliou@qq.com>
2019-02-19 23:14:16 +0800

    Update fool.java

--- Prime for array/sieve of eratosthenes/fool.java
+++ Prime for array/sieve of eratosthenes/fool.java
@@ -1,13 +1,12 @@
 static int[] easyEratosthenes(int[] a) {
 	boolean[] flags = new boolean[a.length]; Arrays.fill(flags, false);
 	ArrayList<Integer> primes = new ArrayList<>((int) Math.sqrt(a.length));
-	for (int i = 2, pi = 0; i < a.length; i++) {
-		if (!flags[i]) {
-			a[pi] = i;
-			primes.add(a[pi]);
-			pi += 1;
-			for (int j = i; j < a.length; j++)
+	for (int i = 0; i < a.length;) {
+		if (!flags[i]) if(isPrime(a[i])) {
+			primes.add(a[i]);
+			for (int j = i + a[i]; j < a.length; j += i)
 				flags[j] = true;
+			i += 1;
 		}
 	}
 	return primes.stream().mapToInt(Integer::intValue).toArray();
