924975fcc9c0152128e3bc2bd5c2e8758380b3bd
AExiaoliou <aexiaoliou@qq.com>
2019-02-19 15:30:47 +0800

    Update .test.java

--- Prime for array/sieve of eratosthenes/.test.java
+++ Prime for array/sieve of eratosthenes/.test.java
@@ -1,7 +1,10 @@
+package com.aexiaoliou.task;
+
 import org.junit.jupiter.api.Test;
 import org.apache.commons.math3.*;
 
 import java.util.Arrays;
+import static java.lang.System.nanoTime;
 
 import static java.lang.Math.*;
 
@@ -9,8 +12,10 @@ class TaskTest {
 	@Test
 	void test() {
 		int[] testdata = new int[1 << 10];
-		for (int i = 0; i < testdata.length; i++) testdata[i] = i;
+		for (int i = 0; i < testdata.length; i++) testdata[i] = i + 2;
+		long begin = nanoTime();
 		Arrays.stream(YourPackage.foolEratosthenes(testdata))
-				.forEach(System.out::println);
+				.forEach(System.out::println); // you can remove print to get accurate result
+		System.out.println("time = " + (nanoTime() - begin) );
 	}
 }
