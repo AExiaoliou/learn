ab1d4d5f94e6cbc95078d0ba36d8079e178c3cc8
AExiaoliou <aexiaoliou@qq.com>
2019-02-23 21:20:27 +0800

    Update trial division.cpp

--- Prime/trial division.cpp
+++ Prime/trial division.cpp
@@ -2,11 +2,22 @@
 
 bool fool(int x) {
     bool isPrime = true;
-    for (int i = 2; i < sqrt(x); i++) 
+    for (int i = 2; i < sqrt(x); i++)  {
         if (x % i == 0) {
             isPrime = false;
             break;
         }
     }
     return isPrime;
+}
+
+bool foolish(int x) {
+    if (x == 2) return true;
+    if (x % 2 == 0) return false;
+    
+    int max = (int) sqrt(x);
+    for (int i = 3; i < max; i += 2) {
+        if (x % i == 0) return false; 
+    }
+    return true;
 }
