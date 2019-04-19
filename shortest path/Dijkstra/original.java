fc755c4958c90d95a26a8ce7558f52e029383255
Hatim <aexiaoliou@qq.com>
2019-04-19 20:32:49 +0800

    Update original.java

--- shortest path/Dijkstra/original.java
+++ shortest path/Dijkstra/original.java
@@ -1,1 +1,45 @@
 
+int n, m;
+Edge[] edges;
+int[][] G;
+boolean[] vis;
+int L[], path[];
+
+void init() {
+	n = cd.nextInt();
+	m = n * (n - 1);
+	edges = new Edge[m];
+	G = new int[n][n - 1];
+	L = new int[n]; Arrays.fill(L, INF);
+	vis = new boolean[n];
+	path = new int[n];
+	for (int i = 0, t = 0; i < n; i++) {
+		for (int j = 0, k = 0; j < n; j++) {
+			int d = cd.nextInt();
+			if (d != 0) {
+				edges[t] = new Edge(i, j, d);
+				G[i][k] = t;
+				t += 1; k += 1;
+			}
+		}
+	}
+}
+void dijkstra() {
+	PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparingInt(Node::getD).reversed());
+	L[0] = 0;
+	queue.add(new Node(0, 0));
+	while (!queue.isEmpty()) {
+		Node x = queue.poll();
+		int node = x.node;
+		if (vis[node]) continue;
+		vis[node] = true;
+		for (int id : G[node]) {
+			Edge e = edges[id];
+			if (L[e.to] > L[node] + e.dis) {
+				L[e.to] = L[node] + e.dis;
+				path[e.to] = id;
+				queue.add(new Node(L[e.to], e.to));
+			}
+		}
+	}
+}
