2a919ccdbdf0b7c7abed8ed577af80557a14f6b8
Hatim <aexiaoliou@qq.com>
2019-04-27 06:25:53 +0800

    Update generalout.java

--- Language_Default_Settings/generalout.java
+++ Language_Default_Settings/generalout.java
@@ -1,1 +1,10 @@
+/*
+* System.out is using BufferedWriter to manage output, which could not manage separator
+/*
 PrintWriter out = new PrintWriter(System.out, true);
+/*
+* If you ignore the second argment 'autoFlush', it will default with false
+* And then, because System.out is null, it will close somewhere
+* So turn 'autoFlush' to true
+* Now, every time you use 'println()' and 'printf()' will flush the output stream 
+*/
