3c23fa5fe3dd46cebb33aa4fb0b37fdb13d8511e
Hatim <aexiaoliou@qq.com>
2019-04-27 06:38:27 +0800

    Update lowbit.cpp

--- Math/lowbit.cpp
+++ Math/lowbit.cpp
@@ -1,14 +1,29 @@
 //The easiest to remember and write
-int lowbit(x) {
+int lowbit(int x) {
     return x & (-x);
 }
 
 //or 
-int lowbit(x) {
+int lowbit(int x) {
     return x&-x; //but not intuitive for reader 
 }
 
 //this guy is the easiest to figure out by yourself
-int lowbit(x) {
+int lowbit(int x) {
     return x & (x ^ (x - 1));
+}
+
+//lowbit can check one num is it equals to 2^n ?
+bool is2(int x) {
+    return lowbit(x) == x;   
+}
+
+//or 2^n - 1
+bool is2_(int x) {
+    return lowbit(x + 1) == x + 1;
+}
+
+//remove last bit 1
+void remove_last_bit1(int & x) {
+    x ^= lowbit(x);   
 }
