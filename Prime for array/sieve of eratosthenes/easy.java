637a88f5377479077e4ef5e843328b1de4ad8120
Hatim <aexiaoliou@qq.com>
2019-04-29 21:41:00 +0800

    Update easy.java

--- Prime for array/sieve of eratosthenes/easy.java
+++ Prime for array/sieve of eratosthenes/easy.java
@@ -1,14 +1,15 @@
-	static int[] easyEratosthenes(int[] a) {
-		boolean[] flags = new boolean[a.length]; Arrays.fill(flags, false);
-		ArrayList<Integer> primes = new ArrayList<>((int) Math.sqrt(a.length));
-		for (int i = 0; i < a.length; i++) {
-			if (!flags[i]) if (isPrime(a[i])) {
-				primes.add(a[i]);
-				for (int j = 0; (j < i) && (a[i] * primes.get(j) < a.length); j++) {
-					flags[a[i] * primes.get(j)] = true;
-					if (a[i] % primes.get(j) == 0) break;
-				}
-			}
+//it actually is eular
+static int[] easyEratosthenes(int[] a) {
+	boolean[] flags = new boolean[a.length]; Arrays.fill(flags, false);
+	ArrayList<Integer> primes = new ArrayList<>((int) Math.sqrt(a.length));
+	for (int i = 0; i < a.length; i++) {
+		if (!flags[i]) if (isPrime(a[i])) {
+			primes.add(a[i]);
 		}
-		return primes.stream().mapToInt(Integer::intValue).toArray();
+		for (int j = 0; (j < i) && (a[i] * primes.get(j) < a.length); j++) {
+			flags[a[i] * primes.get(j)] = true;
+			if (a[i] % primes.get(j) == 0) break;
+		}
 	}
+	return primes.stream().mapToInt(Integer::intValue).toArray();
+}
